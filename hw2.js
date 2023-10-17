ba2184f5dc78568e3d7fec92dd492d1757b13347
Pearl Osborne <pearlosborne@hotmail.com>
2023-10-16 19:53:56 -0500

    finished except error in ex 3

--- hw2.js
+++ hw2.js
@@ -104,3 +104,15 @@ const something = async () => {
     
 }
 something()
+
+
+// https://www.codewars.com/kata/523b4ff7adca849afe000035/train/javascript
+function greet() {return "hello world!"}
+
+// https://www.codewars.com/kata/55a2d7ebe362935a210000b2/train/javascript
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      return Math.min(... args)
+      
+    }
+  }
